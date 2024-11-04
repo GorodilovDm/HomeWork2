e066ddb7d6f0f52e935688fcaec85ff3cbf572f5
GorodilovDm <gorodilovdm@gmail.com>
2024-11-04 11:34:57 +0700

    Update modul_2_4.py небольшое ускорение

--- modul_2_4.py
+++ modul_2_4.py
@@ -6,8 +6,11 @@ for i in numbers:
     is_prime = True
     if i == 1:
         continue
-    for j in range(2, i):
-        if i % j == 0:
+    for j in range(3, i, 2):
+        if i % 2 == 0: # проверяем на четность (четные числа не являются простыми, кроме 2)
+            is_prime = False
+            break
+        if i % j == 0: # проверяем остаток от деления только на нечетные числа (для ускорения)
             is_prime = False
             break
     if is_prime:
