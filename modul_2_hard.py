9ae9ca5dc0b15fcd5d26d182376fc41c317ad5a0
GorodilovDm <gorodilovdm@gmail.com>
2024-11-05 18:19:13 +0700

    Add files via upload

---
+++ modul_2_hard.py
@@ -0,0 +1,11 @@
+import random
+
+one_rnd = random.randint(3, 20)
+result = []
+for i in range(1, one_rnd):
+    for j in range(1 + i, one_rnd):
+        if one_rnd % (i + j) == 0:
+            result.append(i)
+            result.append(j)
+print(f'{one_rnd} - число из первой вставки')
+print(*result, ' - нужный пароль', sep='')
